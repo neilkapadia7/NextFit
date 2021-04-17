47869695bd4b5e6d1599706c6e30c89c40e5a44a
Neil Kapadia <neilkapadia7@gmail.com>
2021-04-17 23:54:31 +0530

    Database Connected

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import connectDB from "./config/db.js"
 
 dotenv.config()
 
 const app = express();
+connectDB();
 
 app.use(express.json())
 
